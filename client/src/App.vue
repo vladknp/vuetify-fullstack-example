239c79505d4be0c038f01c27dd82aacaaa5989d2
Vlad Nazarchuk <knplink@gmail.com>
2022-11-29 19:00:12 +0200

    fix table display

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,22 +2,17 @@
   <v-app>
     <v-card flat>
       <v-tabs v-model="tab" align-with-title>
-        <v-tab>Клієнт</v-tab>
-        <v-tab>Додаткова інформація</v-tab>
+        <v-tab v-for="tab in tables" :key="tab.name">{{ tab.name }}</v-tab>
       </v-tabs>
-      <v-tabs-items v-model="tab">
-        <v-tab-item v-for="table in tables" :key="table.id">
-          <v-card flat>
-            <v-card-text>
-              <v-data-table :headers="table.headers" :items="table.clients">
-                <template #[`item.phone`]="props">
-                  <input v-model="props.item.phone" />
-                </template>
-              </v-data-table>
-            </v-card-text>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+      <v-card flat>
+        <v-card-text>
+          <v-data-table :headers="headers" :items="clients">
+            <template #[`item.phone`]="props">
+              <input v-model="props.item.phone" />
+            </template>
+          </v-data-table>
+        </v-card-text>
+      </v-card>
     </v-card>
   </v-app>
 </template>
@@ -30,7 +25,7 @@ export default {
       tab: null,
       tables: [
         {
-          id: 0,
+          name: "Клієнт",
           headers: [
             {
               text: "Ім`я",
@@ -48,21 +43,9 @@ export default {
               value: "inn",
             },
           ],
-          clients: [
-            {
-              name: "Лован",
-              phone: "+380971234567",
-              inn: 1234567890,
-            },
-            {
-              name: "Рюмер",
-              phone: "+380972234567",
-              inn: 2234567890,
-            },
-          ],
         },
         {
-          id: 1,
+          name: "Додаткова інформація",
           headers: [
             {
               text: "Код ІНН",
@@ -85,24 +68,34 @@ export default {
               value: "numOfPassport",
             },
           ],
-          clients: [
-            {
-              inn: "1234567890",
-              residence: "м. Львів, вул. Миру 8",
-              regPlace: "м. Львів, вул. Миру 8",
-              numOfPassport: 123456,
-            },
-            {
-              inn: "2234567890",
-              residence: "м. Київ, вул. Дмитрівська 28, кв. 5",
-              regPlace: "м. Київ, вул. Дмитрівська 28, кв. 5",
-              numOfPassport: 223456,
-            },
-          ],
+        },
+      ],
+      clients: [
+        {
+          id: 0,
+          name: "Лован",
+          phone: "+380971122334",
+          inn: 1234567890,
+          residence: "м. Львів, вул. Миру 8",
+          regPlace: "м. Львів, вул. Миру 8",
+          numOfPassport: 123456,
+        },
+        {
+          id: 1,
+          name: "Рюмер",
+          phone: "+380975566778",
+          inn: 2234567890,
+          residence: "м. Київ, вул. Дмитрівська 28, кв. 5",
+          regPlace: "м. Київ, вул. Дмитрівська 28, кв. 5",
+          numOfPassport: 223456,
         },
       ],
     };
   },
+  computed: {
+    headers() {
+      return this.tables[this.tab]?.headers || [];
+    },
   computed: {},
   methods: {},
 };
